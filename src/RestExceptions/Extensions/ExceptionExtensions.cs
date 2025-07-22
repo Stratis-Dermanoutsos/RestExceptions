f432535f03595f299bbcc099a27609ee2fd57b89
Stratis Dermanoutsos <stratis450@gmail.com>
2025-07-22 10:13:17 +0300

    Keep old exception's type

--- src/RestExceptions/Extensions/ExceptionExtensions.cs
+++ src/RestExceptions/Extensions/ExceptionExtensions.cs
@@ -9,27 +9,35 @@ public static class ExceptionExtensions
     /// <returns>A <see cref="RestException"/> that corresponds to the type of the provided exception.</returns>
     public static RestException MapToRestException(this Exception exception)
     {
+        if (exception is RestException restException)
+        {
+            return restException;
+        }
+
+        var extensions = new Dictionary<string, object?>
+        {
+            { "originalException", exception.GetType().Name }
+        };
         return exception switch
         {
-            RestException restException => restException,
             // 404 - Not Found
             ArgumentNullException
                 or ArgumentOutOfRangeException
                 or DirectoryNotFoundException
                 or FileNotFoundException
-                or KeyNotFoundException => new NotFoundRestException(exception.Message),
+                or KeyNotFoundException => new NotFoundRestException(exception.Message, extensions),
             // 400 - Bad Request
-            ArgumentException => new BadRequestRestException(exception.Message),
+            ArgumentException => new BadRequestRestException(exception.Message, extensions),
             // 403 - Forbidden
             AccessViolationException
-                or UnauthorizedAccessException => new ForbiddenRestException(exception.Message),
+                or UnauthorizedAccessException => new ForbiddenRestException(exception.Message, extensions),
             // 405 - Method Not Allowed
-            InvalidOperationException => new MethodNotAllowedRestException(exception.Message),
+            InvalidOperationException => new MethodNotAllowedRestException(exception.Message, extensions),
             // 501 - Not Implemented
             NotImplementedException
-                or TypeLoadException => new NotImplementedRestException(exception.Message),
+                or TypeLoadException => new NotImplementedRestException(exception.Message, extensions),
             // 500 - Internal Server Error (Default)
-            _ => new InternalServerErrorRestException(exception.Message),
+            _ => new InternalServerErrorRestException(exception.Message, extensions)
         };
     }
 }
